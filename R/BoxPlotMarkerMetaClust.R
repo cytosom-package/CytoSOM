bd6a6ced71432bc920e4f799152c97734f56afcb
gautierstoll <gautier.stoll@gmail.com>
2020-03-23 15:20:00 +0100

    BoxPlotMarkerMetaClust doc

--- R/BoxPlotMarkerMetaClust.R
+++ R/BoxPlotMarkerMetaClust.R
@@ -1,9 +1,19 @@
-
-## User tool: Box plot of metacluster, For a given marker.
-## treatmentTable should be a dataframe with two column: "Treatment", "files". Robust specifies either Tukey/lm or Dunn (non adjusted p-values).
-## ClustHeat=FALSE for no clustering on heatmap.
-BoxPlotMarkerMetaClust = function(TreeMetaCl,Title,treatmentTable,ControlTreatment,BottomMargin,Marker,Robust=TRUE,ClustHeat=TRUE) {
+#' Box plot of maker MFI, for the different meta-cluster, in a PDF file.
+#' @param TreeMetaCl a FlowSOM tree
+#' @param Title Prefix of the pdf file name
+#' @param treatmentTable  data frame containing a column 'files' and a column 'Treatment' (case sensitive)
+#' @param Controltreatment the name of the control treatment used for statistics
+#' @param BottomMargin size of the boxplots bottom margins, in order to be readable
+#' @param Marker marker name
+#' @param Robust if TRUE, robust statistics is applied (dunn), otherwise linear-model/Tukey
+#' @param ClustHeat if TRUE, heatmaps are clustered
+#' @return a list containing MFI, pairwise comparison p-values, controle vs treatment p-values
+#' @export
+BoxPlotMarkerMetaClust = function(TreeMetaCl,Title,treatmentTable,
+                                  ControlTreatment,BottomMargin,Marker,Robust=TRUE,ClustHeat=TRUE) {
     MarkerIndex = which(TreeMetaCl$fSOMTree$prettyColnames == Marker)
     if (length(MarkerIndex) < 1) {stop("No marker ",Marker," in data")} else {
-    BoxPlotMetaClustFull(TreeMetaCl,Title,treatmentTable,ControlTreatment,BottomMargin,yLab="",Norm=FALSE,Marker,Robust,ClustHeat) }
+    BoxPlotMetaClustFull(TreeMetaCl,Title,treatmentTable,ControlTreatment,
+                         BottomMargin,yLab="",Norm=FALSE,Marker,Robust,ClustHeat) }
+    ## change the name "Size" in "MFI"
 }
