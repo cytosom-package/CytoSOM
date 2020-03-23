90a62cb1662c92a567d81faf7c29ac0749627769
gautierstoll <gautier.stoll@gmail.com>
2020-03-23 16:03:02 +0100

    BoxPlotMetaClust doc

--- R/BoxPlotMetaClust.R
+++ R/BoxPlotMetaClust.R
@@ -2,6 +2,18 @@
 ## TreatmentTable should be a dataframe with two column: "Treatment", "files" (a third one with column "NormalizationFactor" if Norm=T).
 ## Robust specifies either Tukey/lm or Dunn (non adjusted p-values).
 ## ClustHeat=FALSE for no clustering on heatmap.
+#' Box plot of maker MFI, for the different meta-cluster with statical analysis, output in a PDF file.
+#' @param TreeMetaCl a FlowSOM tree
+#' @param Title Prefix of the pdf file name
+#' @param treatmentTable  data frame containing a column 'files' and a column 'Treatment' (case sensitive)
+#' @param Controltreatment the name of the control treatment used for statistics
+#' @param BottomMargin size of the boxplots bottom margins, in order to be readable
+#' @param ylab name of y-axis of boxplots (eg 'population size')
+#' @param Norm if true, sizes are normlized according to column 'NormalizationFactor' of the treatment table. Otherwise, percentage is used
+#' @param Robust if TRUE, robust statistics is applied (dunn), otherwise linear-model/Tukey
+#' @param ClustHeat if TRUE, heatmaps are clustered
+#' @return a list containing sizes, pairwise comparison p-values, controle vs treatment p-values
+#' @export
 BoxPlotMetaClust = function(TreeMetaCl,Title,treatmentTable,ControlTreatment,BottomMargin,yLab,Norm=FALSE,Robust = TRUE,ClustHeat=TRUE) {
    BoxPlotMetaClustFull(TreeMetaCl,Title,treatmentTable,ControlTreatment,BottomMargin,yLab,Norm,Marker="",Robust,ClustHeat)
 }
