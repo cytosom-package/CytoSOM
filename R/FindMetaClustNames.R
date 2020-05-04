9445a364b5b831f190cc18b6d586a430a4de34da
gautierstoll <gautier.stoll@gmail.com>
2020-05-05 01:16:52 +0200

    Update meta-cluster renaming functions

--- R/FindMetaClustNames.R
+++ R/FindMetaClustNames.R
@@ -1,14 +1,14 @@
 #' Find the names of metaclusters from a sub-name
-#' @param subNames list of sub-part of a meta-cluster name, for meta-cluster number preceeding its name, use "^number_"
+#' @param subNames list of sub-part of a meta-cluster name, for meta-cluster number preceeding its name
 #' @param TreeMetaCl FlowSOM tree with meta-clusters, constructed within buildFSOMTree (from the data of FSOMData)
+#' @param start If TRUE, match subnames with the start of meta-cluster names
 #' @return a vector of names
 #' @export
 
-FindMetaClustNames <- function(subNames,TreeMetaCl)
-{ if (length(subNames) == 1) {
-  unique(TreeMetaCl$metaCl)[grep(gsub("+","\\+",subNames,fixed=T),unique(TreeMetaCl$metaCl),T)]}
+FindMetaClustNames <- function(subNames,TreeMetaCl,start=F)
+{ if (length(subNames) == 1) {unique(TreeMetaCl$metaCl)[grep(subNames,unique(TreeMetaCl$metaCl),fixed=T)]}
   else
-  {
-  Reduce(intersect,lapply(subNames,function(nm){unique(TreeMetaCl$metaCl)[grep(gsub("+","\\+",nm,fixed=T),unique(TreeMetaCl$metaCl),T)]}))
-  }
+  {Reduce(intersect,lapply(subNames,function(nm){
+    unique(TreeMetaCl$metaCl)[grep(nm,unique(TreeMetaCl$metaCl),fixed=T)]}))}
+  ####
 }
--- R/MetaClusterNaming.R
+++ R/MetaClusterNaming.R
@@ -1,31 +1,60 @@
-#' Naming of meta-cluster from median and quartiles
+#' Naming of meta-cluster, based on quartile if CutOff is not provided
 #' @param TreeMetaCl FlowSOM tree with meta-clusters, contructed for buildFSOMTree
 #' @param Markers vector of markers used for naming
-#' @return data frame with cluster number, non-robust name, robust name and short robust names
+#' @param Levels number of level for naming: 2 -> -/+ (1/2-4 quartiles), 3-> -/low/high (1/2-3/4 quartiles), 4-> -/low/int/high (1/2/3/4 quartiles)
+#' @param CutOff matrix for optional cut off values for naming: one line for each markers (in the same order), each column for separating levels. If NULL, quartiles are used
+#' @param Short is true, low is not represented with level=3, int is not represented if level=4
+#' @return data frame with meta-cluster cluster oldName and newName.
 #' @export
 
-MetaClusterNaming <- function(TreeMetaCl,Markers)
+MetaClusterNaming <- function(TreeMetaCl,Markers,Levels,CutOff=NULL,Short=FALSE)
 {
-    MarkerIn = sapply(Markers,function(Marker){length(which(TreeMetaCl$fSOMTree$prettyColnames == Marker))})
-    Markers = Markers[which(MarkerIn > 0)]
-    print(paste("Use Marker",Markers))
-
-
-    metaClustListName=lapply(unique(TreeMetaCl$metaCl),function(metaClust){
-            clusterList=which(TreeMetaCl$metaCl == metaClust)
-            metaClustIndices=unlist(sapply(clusterList,function(cluster){which(TreeMetaCl$fSOMTree$map$mapping[,1] == cluster)}))
-            nameList = sapply(Markers,function(Marker){
-                MarkerIndex=which(TreeMetaCl$fSOMTree$prettyColnames == Marker)
-                metaClustMedian=median(TreeMetaCl$fSOMTree$data[metaClustIndices,MarkerIndex],na.rm=T)
+    Levels = as.integer(Levels)
+    if ((Levels < 2) | (Levels > 4)) {stop("Not the right amount of levels")}
+    MarkerIn = Markers[which(sapply(Markers,function(Marker){length(which(TreeMetaCl$fSOMTree$prettyColnames == Marker))})>0)]
+    if (length(setdiff(Markers,MarkerIn))>0) {stop(paste("Don't find markers ",paste(setdiff(Markers,MarkerIn),collapse = " "),sep=""))}
+    if (!is.null(CutOff)) {
+        if (length(CutOff[,1] != length(Markers))) {stop("not the number of CutOff lines compares to Markers")}
+        if (length(CutOff[1,]) != Levels) {steop("number of CutOffs no equal to Levels")}}
+    metaClustNewNames=lapply(unique(TreeMetaCl$metaCl),function(metaClust){
+        clusterList=which(TreeMetaCl$metaCl == metaClust)
+        metaClustIndices=unlist(sapply(clusterList,function(cluster){which(TreeMetaCl$fSOMTree$map$mapping[,1] == cluster)}))
+        nameList = sapply(Markers,function(Marker){
+            MarkerIndex=which(TreeMetaCl$fSOMTree$prettyColnames == Marker)
+            metaClustMedian=median(TreeMetaCl$fSOMTree$data[metaClustIndices,MarkerIndex],na.rm=T)
+            if(is.null(CutOff)) {
                 vectQuantiles=quantile(TreeMetaCl$fSOMTree$data[,MarkerIndex],na.rm=T)
-                nonRobustName = c(paste(Marker,"-",sep=""),paste(Marker,"+",sep=""))[as.numeric(metaClustMedian > vectQuantiles[3])+1]
-                robustName = c(paste(Marker,"-",sep=""),paste(Marker,"med",sep=""),paste(Marker,"+",sep=""))[as.numeric(metaClustMedian > vectQuantiles[2])+as.numeric(metaClustMedian > vectQuantiles[4])+1]
-                robustNameNoMed= c(paste(Marker,"-",sep=""),"",paste(Marker,"+",sep=""))[as.numeric(metaClustMedian > vectQuantiles[2])+as.numeric(metaClustMedian > vectQuantiles[4])+1]
-                return(c(nonRobustName,robustName,robustNameNoMed))
-            })
-            c(metaClust,apply(nameList,1,function(l){paste(l,collapse="")}))
+                if (Levels == 2) {namingMarker = paste(Marker,c("-","+"),sep="")[as.numeric(metaClustMedian > vectQuantiles[2])+1]}
+                else if (Levels == 3) {
+                    if (Short) {namingMarker = c(paste(Marker,"-",sep=""),"",paste(Marker,"high",sep=""))[
+                        as.numeric(metaClustMedian > vectQuantiles[2]) + as.numeric(metaClustMedian > vectQuantiles[4]) +1]}
+                    else {namingMarker = c(paste(Marker,"-",sep=""),paste(Marker,"low",sep=""),paste(Marker,"high",sep=""))[
+                        as.numeric(metaClustMedian > vectQuantiles[2]) + as.numeric(metaClustMedian > vectQuantiles[4]) +1]}}
+                else {if (Short) {namingMarker = c(paste(Marker,"-",sep=""),paste(Marker,"low",sep=""),"",paste(Marker,"high",sep=""))[
+                    as.numeric(metaClustMedian > vectQuantiles[2]) +
+                        as.numeric(metaClustMedian > vectQuantiles[3]) + as.numeric(metaClustMedian > vectQuantiles[4]) +1]}
+                    else {namingMarker = c(paste(Marker,"-",sep=""),paste(Marker,"low",sep=""),paste(Marker,"int",sep=""),paste(Marker,"high",sep=""))[
+                        as.numeric(metaClustMedian > vectQuantiles[2]) +
+                            as.numeric(metaClustMedian > vectQuantiles[3]) + as.numeric(metaClustMedian > vectQuantiles[4]) +1]}}}
+            else {
+                cutOffLine = CutOff[which(Markers == Marker),]
+                if (Levels == 2) {namingMarker = paste(Marker,c("-","+"),sep="")[as.numeric(metaClustMedian > cutOffLine)+1]}
+                else if (Levels == 3) {
+                    if (Short) {namingMarker = c(paste(Marker,"-",sep=""),"",paste(Marker,"high",sep=""))[
+                        as.numeric(metaClustMedian > cutOffLine[1]) + as.numeric(metaClustMedian > cutOffLine[2]) +1]}
+                    else {namingMarker = c(paste(Marker,"-",sep=""),paste(Marker,"low",sep=""),paste(Marker,"high",sep=""))[
+                        as.numeric(metaClustMedian > cutOffLine[1]) + as.numeric(metaClustMedian > cutOffLine[2]) +1]}}
+                else {if (Short) {namingMarker = c(paste(Marker,"-",sep=""),paste(Marker,"low",sep=""),"",paste(Marker,"high",sep=""))[
+                    as.numeric(metaClustMedian > cutOffLine[1]) +
+                        as.numeric(metaClustMedian > cutOffLine[2]) + as.numeric(metaClustMedian > cutOffLine[3]) +1]}
+                    else {namingMarker = c(paste(Marker,"-",sep=""),paste(Marker,"low",sep=""),paste(Marker,"int",sep=""),paste(Marker,"high",sep=""))[
+                        as.numeric(metaClustMedian > cutOffLine[1]) +
+                            as.numeric(metaClustMedian > cutOffLine[2]) + as.numeric(metaClustMedian > cutOffLine[3]) +1]}}}
+            return(namingMarker)
+        })
+        nameMeta = paste(nameList,collapse="")
+        return(nameMeta)
     })
-    metaClustDF=as.data.frame(do.call(rbind,metaClustListName),stringsAsFactors=F)
-    names(metaClustDF)=c("number","nonRobustName","robustName","shortRobustName")
+    metaClustDF=data.frame(oldName=unique(TreeMetaCl$metaCl),newName = unlist(metaClustNewNames))
     return(metaClustDF)
 }
