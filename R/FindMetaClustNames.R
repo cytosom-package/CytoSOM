28102de2828287bcf505bde3eb68b54ef28f303b
gautierstoll <gautier.stoll@gmail.com>
2020-05-08 12:02:01 +0200

    New version of FindMetaClustName

--- R/FindMetaClustNames.R
+++ R/FindMetaClustNames.R
@@ -1,14 +1,27 @@
-#' Find the names of metaclusters from a sub-name
-#' @param subNames list of sub-part of a meta-cluster name, for meta-cluster number preceeding its name
+#' Find each name of metaclusters that contains all sub-names
+#' @param subNames list of sub-part of a meta-cluster name
 #' @param TreeMetaCl FlowSOM tree with meta-clusters, constructed within buildFSOMTree (from the data of FSOMData)
 #' @param start If TRUE, match subnames with the start of meta-cluster names
 #' @return a vector of names
 #' @export
 
 FindMetaClustNames <- function(subNames,TreeMetaCl,start=F)
-{ if (length(subNames) == 1) {unique(TreeMetaCl$metaCl)[grep(subNames,unique(TreeMetaCl$metaCl),fixed=T)]}
+{
+  if (start) {
+    if (length(subNames) == 1) {
+      unique(TreeMetaCl$metaCl)[substr(unique(TreeMetaCl$metaCl),start=0,stop=nchar(subNames)) == subNames]
+      }
+  else
+  {
+    Reduce(intersect,lapply(subNames,function(nm){
+      unique(TreeMetaCl$metaCl)[substr(unique(TreeMetaCl$metaCl),start=0,stop=nchar(nm)) == nm]}))
+  }
+}
+else
+{
+  if (length(subNames) == 1) {unique(TreeMetaCl$metaCl)[grep(subNames,unique(TreeMetaCl$metaCl),fixed=T)]}
   else
   {Reduce(intersect,lapply(subNames,function(nm){
     unique(TreeMetaCl$metaCl)[grep(nm,unique(TreeMetaCl$metaCl),fixed=T)]}))}
-  ####
+  }
 }
